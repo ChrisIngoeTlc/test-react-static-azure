c1ebb0e61119a37b10902aa4ed9a3ed3c15fae36
ChrisIngoeTlc <christopher.ingoe@thomsonlocal.com>
2020-07-15 16:28:34 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import Products from './components/products';
 
 class App extends Component {
   render() {
-    return (
-      <Products products={this.state.products} />
-    )
+    return <p>Hi</p>;
   }
 
   state = {
-    products: []
+    products: [],
   };
 
-  componentDidMount() {
-    fetch('https://staticwebappwolfgang.azurewebsites.net/api/Function1')
-      .then(res => res.json())
-      .then((data) => {
-        this.setState({ products: data })
-      })
-      .catch(console.log)
-  }
+  componentDidMount() {}
 }
 
 export default App;
