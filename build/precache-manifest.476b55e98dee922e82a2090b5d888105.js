63b4bb2ad0ff823e445d4fda8563ce61bd7759e9
ChrisIngoeTlc <christopher.ingoe@thomsonlocal.com>
2020-07-21 09:45:49 +0100

    Add build output to the repository (I know, I know, not good. Just testing someting out!)

---
+++ build/precache-manifest.476b55e98dee922e82a2090b5d888105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a20e766f9a73f231f83e819de97343",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6409d2cf65d722943be",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0349878ac060cacff6a8",
+    "url": "/static/js/2.8f2a2cdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f2a2cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6409d2cf65d722943be",
+    "url": "/static/js/main.5cbd3298.chunk.js"
+  },
+  {
+    "revision": "9f508dc183ab2e22434d",
+    "url": "/static/js/runtime-main.8337eec2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
